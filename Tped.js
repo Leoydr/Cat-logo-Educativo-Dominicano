322191cc3f1356dfa0022f7cac57f2fd18c713d2
Yoel Del Rosario <yoeldr8@gmail.com>
2025-09-04 16:50:38 -0400

    Se agrega el proyecto

---
+++ Tped.js
@@ -0,0 +1,307 @@
+
+const instituciones = [
+  {
+    nombre: "INFOTEP",
+    descripcion: "Portal oficial de INFOTEP. Formación académica y técnica en República Dominicana.",
+    enlace: "https://infotepvirtual.com/",
+    logo: "INFOTEP.png"
+  },
+  {
+    nombre: "INAFOCAM",
+    descripcion: "Portal oficial de INAFOCAM. Formación académica y técnica en República Dominicana.",
+    enlace: "https://www.google.com/search?q=INAFOCAM",
+    logo: "INAFOCAM.png"
+  },
+  {
+    nombre: "ISFODOSU",
+    descripcion: "Portal oficial de ISFODOSU. Formación académica y técnica en República Dominicana.",
+    enlace: "https://www.google.com/search?q=ISFODOSU",
+    logo: "ISFODOSU.png"
+  },
+  {
+    nombre: "ITLA",
+    descripcion: "Portal oficial de ITLA. Formación académica y técnica en República Dominicana.",
+    enlace: "https://www.google.com/search?q=ITLA",
+    logo: "ITLA.png"
+  },
+  {
+    nombre: "PUCMM",
+    descripcion: "Portal oficial de PUCMM. Formación académica y técnica en República Dominicana.",
+    enlace: "https://www.google.com/search?q=PUCMM",
+    logo: "PUCMM.png"
+  },
+  {
+    nombre: "UNPHU",
+    descripcion: "Portal oficial de UNPHU. Formación académica y técnica en República Dominicana.",
+    enlace: "https://www.google.com/search?q=UNPHU",
+    logo: "UNPHU.png"
+  },
+  {
+    nombre: "UNAPEC",
+    descripcion: "Portal oficial de UNAPEC. Formación académica y técnica en República Dominicana.",
+    enlace: "https://www.google.com/search?q=UNAPEC",
+    logo: "UNAPEC.png"
+  },
+  {
+    nombre: "UASD",
+    descripcion: "Portal oficial de UASD. Formación académica y técnica en República Dominicana.",
+    enlace: "https://www.google.com/search?q=UASD",
+    logo: "UASD.png"
+  },
+  {
+    nombre: "INTEC",
+    descripcion: "Portal oficial de INTEC. Formación académica y técnica en República Dominicana.",
+    enlace: "https://www.google.com/search?q=INTEC",
+    logo: "INTEC.png"
+  },
+  {
+    nombre: "UFHEC",
+    descripcion: "Portal oficial de UFHEC. Formación académica y técnica en República Dominicana.",
+    enlace: "https://www.google.com/search?q=UFHEC",
+    logo: "UFHEC.png"
+  },
+  {
+    nombre: "UCSD",
+    descripcion: "Portal oficial de UCSD. Formación académica y técnica en República Dominicana.",
+    enlace: "https://www.google.com/search?q=UCSD",
+    logo: "UCSD.png"
+  },
+  {
+    nombre: "UCNE",
+    descripcion: "Portal oficial de UCNE. Formación académica y técnica en República Dominicana.",
+    enlace: "https://www.google.com/search?q=UCNE",
+    logo: "UCNE.png"
+  },
+  {
+    nombre: "UNICARIBE",
+    descripcion: "Portal oficial de UNICARIBE. Formación académica y técnica en República Dominicana.",
+    enlace: "https://www.google.com/search?q=UNICARIBE",
+    logo: "UNICARIBE.png"
+  },
+  {
+    nombre: "UNEV",
+    descripcion: "Portal oficial de UNEV. Formación académica y técnica en República Dominicana.",
+    enlace: "https://www.google.com/search?q=UNEV",
+    logo: "UNEV.png"
+  },
+  {
+    nombre: "UNIBE",
+    descripcion: "Portal oficial de UNIBE. Formación académica y técnica en República Dominicana.",
+    enlace: "https://www.google.com/search?q=UNIBE",
+    logo: "UNIBE.png"
+  },
+  {
+    nombre: "UTESA",
+    descripcion: "Portal oficial de UTESA. Formación académica y técnica en República Dominicana.",
+    enlace: "https://www.google.com/search?q=UTESA",
+    logo: "UTESA.png"
+  },
+  {
+    nombre: "UAPA",
+    descripcion: "Portal oficial de UAPA. Formación académica y técnica en República Dominicana.",
+    enlace: "https://www.google.com/search?q=UAPA",
+    logo: "UAPA.png"
+  },
+  {
+    nombre: "UNAD",
+    descripcion: "Portal oficial de UNAD. Formación académica y técnica en República Dominicana.",
+    enlace: "https://www.google.com/search?q=UNAD",
+    logo: "UNAD.png"
+  },
+  {
+    nombre: "UNIREMHOS",
+    descripcion: "Portal oficial de UNIREMHOS. Formación académica y técnica en República Dominicana.",
+    enlace: "https://www.google.com/search?q=UNIREMHOS",
+    logo: "UNIREMHOS.png"
+  },
+  {
+    nombre: "UCATECI",
+    descripcion: "Portal oficial de UCATECI. Formación académica y técnica en República Dominicana.",
+    enlace: "https://www.google.com/search?q=UCATECI",
+    logo: "UCATECI.png"
+  },
+  {
+    nombre: "UCATEBA",
+    descripcion: "Portal oficial de UCATEBA. Formación académica y técnica en República Dominicana.",
+    enlace: "https://www.google.com/search?q=UCATEBA",
+    logo: "UCATEBA.png"
+  },
+  {
+    nombre: "UCDEP",
+    descripcion: "Portal oficial de UCDEP. Formación académica y técnica en República Dominicana.",
+    enlace: "https://www.google.com/search?q=UCDEP",
+    logo: "UCDEP.png"
+  },
+  {
+    nombre: "UCE",
+    descripcion: "Portal oficial de UCE. Formación académica y técnica en República Dominicana.",
+    enlace: "https://www.google.com/search?q=UCE",
+    logo: "UCE.png"
+  },
+  {
+    nombre: "UNICA",
+    descripcion: "Portal oficial de UNICA. Formación académica y técnica en República Dominicana.",
+    enlace: "https://www.google.com/search?q=UNICA",
+    logo: "UNICA.png"
+  },
+  {
+    nombre: "UNEFA",
+    descripcion: "Portal oficial de UNEFA. Formación académica y técnica en República Dominicana.",
+    enlace: "https://www.google.com/search?q=UNEFA",
+    logo: "UNEFA.png"
+  },
+  {
+    nombre: "UOD",
+    descripcion: "Portal oficial de UOD. Formación académica y técnica en República Dominicana.",
+    enlace: "https://www.google.com/search?q=UOD",
+    logo: "UOD.png"
+  },
+  {
+    nombre: "UAFAM",
+    descripcion: "Portal oficial de UAFAM. Formación académica y técnica en República Dominicana.",
+    enlace: "https://www.google.com/search?q=UAFAM",
+    logo: "UAFAM.png"
+  },
+  {
+    nombre: "UTE",
+    descripcion: "Portal oficial de UTE. Formación académica y técnica en República Dominicana.",
+    enlace: "https://www.google.com/search?q=UTE",
+    logo: "UTE.png"
+  },
+  {
+    nombre: "OYM",
+    descripcion: "Portal oficial de OYM. Formación académica y técnica en República Dominicana.",
+    enlace: "https://www.google.com/search?q=OYM",
+    logo: "OYM.png"
+  },
+  {
+    nombre: "FLACSO",
+    descripcion: "Portal oficial de FLACSO. Formación académica y técnica en República Dominicana.",
+    enlace: "https://www.google.com/search?q=FLACSO",
+    logo: "FLACSO.png"
+  },
+  {
+    nombre: "UNNATEC",
+    descripcion: "Portal oficial de UNNATEC. Formación académica y técnica en República Dominicana.",
+    enlace: "https://www.google.com/search?q=UNNATEC",
+    logo: "UNNATEC.png"
+  },
+  {
+    nombre: "ITESA",
+    descripcion: "Portal oficial de ITESA. Formación académica y técnica en República Dominicana.",
+    enlace: "https://www.google.com/search?q=ITESA",
+    logo: "ITESA.png"
+  },
+  {
+    nombre: "ISED",
+    descripcion: "Portal oficial de ISED. Formación académica y técnica en República Dominicana.",
+    enlace: "https://www.google.com/search?q=ISED",
+    logo: "ISED.png"
+  },
+  {
+    nombre: "IPL",
+    descripcion: "Portal oficial de IPL. Formación académica y técnica en República Dominicana.",
+    enlace: "https://www.google.com/search?q=IPL",
+    logo: "IPL.png"
+  },
+  {
+    nombre: "CENATEC",
+    descripcion: "Portal oficial de CENATEC. Formación académica y técnica en República Dominicana.",
+    enlace: "https://www.google.com/search?q=CENATEC",
+    logo: "CENATEC.png"
+  },
+  {
+    nombre: "IDECAE",
+    descripcion: "Portal oficial de IDECAE. Formación académica y técnica en República Dominicana.",
+    enlace: "https://www.google.com/search?q=IDECAE",
+    logo: "IDECAE.png"
+  },
+  {
+    nombre: "INTA",
+    descripcion: "Portal oficial de INTA. Formación académica y técnica en República Dominicana.",
+    enlace: "https://www.google.com/search?q=INTA",
+    logo: "INTA.png"
+  },
+  {
+    nombre: "ICA",
+    descripcion: "Portal oficial de ICA. Formación académica y técnica en República Dominicana.",
+    enlace: "https://www.google.com/search?q=ICA",
+    logo: "ICA.png"
+  },
+  {
+    nombre: "INTEDO",
+    descripcion: "Portal oficial de INTEDO. Formación académica y técnica en República Dominicana.",
+    enlace: "https://www.google.com/search?q=INTEDO",
+    logo: "INTEDO.png"
+  },
+  {
+    nombre: "SECOM",
+    descripcion: "Portal oficial de SECOM. Formación académica y técnica en República Dominicana.",
+    enlace: "https://www.google.com/search?q=SECOM",
+    logo: "SECOM.png"
+  },
+  {
+    nombre: "FUCLA",
+    descripcion: "Portal oficial de FUCLA. Formación académica y técnica en República Dominicana.",
+    enlace: "https://www.google.com/search?q=FUCLA",
+    logo: "FUCLA.png"
+  },
+  {
+    nombre: "FormandoteYa",
+    descripcion: "Portal oficial de FormandoteYa. Formación académica y técnica en República Dominicana.",
+    enlace: "https://www.google.com/search?q=FormandoteYa",
+    logo: "FormandoteYa.png"
+  },
+  {
+    nombre: "Jensly International School",
+    descripcion: "Portal oficial de Jensly International School. Formación académica y técnica en República Dominicana.",
+    enlace: "https://www.google.com/search?q=Jensly+International+School",
+    logo: "Jensly International School.png"
+  },
+  {
+    nombre: "Promapec",
+    descripcion: "Portal oficial de Promapec. Formación académica y técnica en República Dominicana.",
+    enlace: "https://www.google.com/search?q=Promapec",
+    logo: "Promapec.png"
+  },
+  {
+    nombre: "ASCA",
+    descripcion: "Portal oficial de ASCA. Formación académica y técnica en República Dominicana.",
+    enlace: "https://www.google.com/search?q=ASCA",
+    logo: "ASCA.png"
+  },
+  {
+    nombre: "IGLOBAL",
+    descripcion: "Portal oficial de IGLOBAL. Formación académica y técnica en República Dominicana.",
+    enlace: "https://www.google.com/search?q=IGLOBAL",
+    logo: "IGLOBAL.png"
+  },
+  {
+    nombre: "IEESL",
+    descripcion: "Portal oficial de IEESL. Formación académica y técnica en República Dominicana.",
+    enlace: "https://www.google.com/search?q=IEESL",
+    logo: "IEESL.png"
+  },
+  {
+    nombre: "INSUTEC",
+    descripcion: "Portal oficial de INSUTEC. Formación académica y técnica en República Dominicana.",
+    enlace: "https://www.google.com/search?q=INSUTEC",
+    logo: "INSUTEC.png"
+  },
+]
+
+const catalogo = document.getElementById("catalogo");
+
+instituciones.forEach(inst => {
+  const card = document.createElement("div");
+  card.className = "card";
+
+  card.innerHTML = `
+    <img src="imagenes/${inst.logo}" alt="Logo de ${inst.nombre}" />
+    <h2>${inst.nombre}</h2>
+    <p>${inst.descripcion}</p>
+    <a href="${inst.enlace}" target="_blank">Ir al sitio</a>
+  `;
+
+  catalogo.appendChild(card);
+});
